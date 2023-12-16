b257f2a52efe34478709496451009a0cd8797bc6
dung204 <acezombiev4@gmail.com>
2023-12-17 00:02:51 +0700

    add content collection for posts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,12 +1,20 @@
 import type { APIContext } from 'astro';
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context: APIContext) {
+  const posts = await getCollection('posts');
+
   return rss({
     title: 'Mantrilogix | Blog',
     description: 'My journey learning Astro',
     site: context.site || 'localhost:4321',
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map(post => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: '<language>en-us</language>',
   });
 }
